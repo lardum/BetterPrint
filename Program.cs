1a6144bbf9a2bc6811e06ed046bd448daa049845
lardum <abajda95@gmail.com>
2025-03-21 16:59:33 +0100

    aa

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using BetterPrint;
 
 // const string path = @"D:\code3\test\HelloWorld\bin\Debug\net8.0\HelloWorld.dll";
-const string path = @"D:\code3\test\HelloWorld\bin\Debug\net8.0\HelloWorld.dll";
+const string path = @"./HelloWorld.dll";
+
 var parser = new Parser(path);
 var metadata = parser.Parse();
